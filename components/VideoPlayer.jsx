2e272652ffb31ad1c455820facc0c669034919e9
Shreyansh kankane <shreyanshguptaphy@gmail.com>
2024-01-06 19:50:25 +0530

    unmute user

--- components/VideoPlayer.jsx
+++ components/VideoPlayer.jsx
@@ -25,7 +25,7 @@ function VideoPlayer() {
       {callAccepted && !callEnded && <div
         className='absolute top-0 left-0 h-full w-full'
       >
-        <video muted playsInline autoPlay ref={userVideo} className='w-full object-fill'></video>
+        <video playsInline autoPlay ref={userVideo} className='w-full object-fill'></video>
 
       </div> }
 
