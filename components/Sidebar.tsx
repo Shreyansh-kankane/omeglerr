a7da6179a497bfe0d2d02dc551f77159373bec29
Shreyansh kankane <shreyanshguptaphy@gmail.com>
2024-01-14 20:59:34 +0530

    experiment-branch

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -9,9 +9,9 @@ function Sidebar({width}:{width:string}) {
     <div className={`hidden md:flex ${width} bg-[#e8ded2] overflow-hidden`}>
       {/* card */}
 
-      <AdsenseComp />
+      {/* <AdsenseComp /> */}
 
-      {/* <div className='w-full h-2/3 m-3 p-4 rounded-md border shadow-xl bg-white '>
+      <div className='w-full h-2/3 m-3 p-4 rounded-md border shadow-xl bg-white '>
         <div className='flex flex-col items-center bg-gray-100 border-none p-2'>
           <Image src={'/chat.jpg'} width={200} height={200} alt='Chatting' className='rounded-xl'/>
         </div>
@@ -22,7 +22,7 @@ function Sidebar({width}:{width:string}) {
         <div className='text-center bg-slate-50 mt-5 italic'>
           <p>Click on New to Start</p>
         </div>
-      </div> */}
+      </div>
     </div>
   )
 }
